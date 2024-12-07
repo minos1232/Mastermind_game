0ce76723eae6b0c0fdb1a0ec9532a33908e1a610
amine <mesbahminos@gmail.com>
2024-12-07 14:53:17 +0100

    add Mastermind class with initialized array for the secret colors

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,1 +1,17 @@
 #Mastermind game
+class Mastermind
+  attr_accessor :game_colors , :color_choices
+  def initialize
+    @game_colors = ['red','blue','green','purple','yellow','orange']
+    @color_choices= Array.new 
+    for i in 0...4 do
+      color_choices[i] = game_colors.sample
+    end
+  end
+
+  def play
+    puts color_choices
+  end
+end
+game = Mastermind.new
+game.play
