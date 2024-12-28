7345aea259e79eb067be10ee4cd7954b973f6fe1
amine <mesbahminos@gmail.com>
2024-12-28 13:49:50 +0100

    add an option to choose between the creator or the guesser in game with initialize def

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,22 +1,23 @@
-#Mastermind game  
+#Mastermind game
 class Mastermind
   attr_accessor :game_colors , :color_choices, :colors_guess
-  def initialize
+  def initialize(choice)
     @game_colors = ['red','blue','green','purple','yellow','orange']
     @color_choices= Array.new
     @colors_guess= Array.new
-    for i in 0...4 do
-      color_choices[i] = game_colors.sample
+    if choice == 1
+      @color_choices = guesser_start
+    else
+      @color_choices = creator_start
     end
   end
 
   def player_guess
-    #colors_guess = Array.new
     for i in 1...5 do 
       puts "guess color number #{i}"
       while true
         guess = gets.chomp
-        if game_colors.any?(guess)  #verify if the color you guessed exists and correct
+        if @game_colors.any?(guess)  #verify if the color you guessed exists and correct
           colors_guess[i-1] = guess
           break
         else
@@ -72,7 +73,38 @@ class Mastermind
       puts color_choices
     end
   end
+
+  private
+  def guesser_start
+    guesser_array = []
+    for i in 0...4 do
+      guesser_array[i] = game_colors.sample
+    end
+    return guesser_array
+  end
+
+  def creator_start
+  loop do
+    puts"enter the 4 colors the computer has to guess,here is an example: red green blue purple"
+    creation= gets.chomp.split
+      if creation.size == 4 && creation.all? { |word| @game_colors.include?(word)}
+        return creation
+      else
+        puts "Error!"
+      end
+    end
+  end
 end
 
-game = Mastermind.new
+puts "Hello!\nif you want be the guesser enter 1\nif you want to be the creator enter 2"
+choice = nil
+loop do
+  choice = gets.chomp.to_i
+  if choice == 1 || choice == 2
+    break
+  else
+    puts "Error! Enter 1 if you want be the guesser enter and 2 if you want to be the creator"
+  end
+end
+game = Mastermind.new(choice)
 game.play
